2582f8e36fec939ec1f0bbd45845c895bdfa30a4
josephonly <joseph062518@gmail.com>
2025-02-17 09:35:21 +0800

    usermanagemnet

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -8,7 +8,7 @@
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-user"></i>
-      <span>User </span>
+      <span>User Management</span>
     </a>
     <ul class="nav submenu">
       <li><a href="group.php">Manage Groups</a> </li>
