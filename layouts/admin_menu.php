f166b6d943986759d425478aefa2512bbb52d9bf
soobink1 <peronamarie9@gmail.com>
2025-02-17 09:30:18 +0800

    change

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -13,10 +13,10 @@
     <ul class="nav submenu">
       <li><a href="group.php">Manage Groups</a> </li>
       <li><a href="users.php">Manage Users</a> </li>
-   </ul>
+    </ul>
   </li>
   <li>
-    <a href="categorie.php" >
+    <a href="categorie.php">
       <i class="glyphicon glyphicon-indent-left"></i>
       <span>Categories</span>
     </a>
@@ -27,12 +27,12 @@
       <span>Products</span>
     </a>
     <ul class="nav submenu">
-       <li><a href="product.php">Manage Products</a> </li>
-       <li><a href="add_product.php">Add Products</a> </li>
-   </ul>
+      <li><a href="product.php">Manage Products</a> </li>
+      <li><a href="add_product.php">Add Products</a> </li>
+    </ul>
   </li>
   <li>
-    <a href="media.php" >
+    <a href="media.php">
       <i class="glyphicon glyphicon-picture"></i>
       <span>Media Files</span>
     </a>
@@ -40,22 +40,46 @@
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-credit-card"></i>
-       <span>Sales</span>
-      </a>
-      <ul class="nav submenu">
-         <li><a href="sales.php">Manage Sales</a> </li>
-         <li><a href="add_sale.php">Add Sale</a> </li>
-     </ul>
+      <span>Sales</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales.php">Manage Sales</a> </li>
+      <li><a href="add_sale.php">Add Sale</a> </li>
+    </ul>
   </li>
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-duplicate"></i>
-       <span>Sales Report</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="sales_report.php">Sales by dates </a></li>
-        <li><a href="monthly_sales.php">Monthly sales</a></li>
-        <li><a href="daily_sales.php">Daily sales</a> </li>
-      </ul>
+      <span>Sales Report</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales_report.php">Sales by dates</a></li>
+      <li><a href="monthly_sales.php">Monthly sales</a></li>
+      <li><a href="daily_sales.php">Daily sales</a> </li>
+    </ul>
+  </li>
+
+  <!-- Date and Time Section -->
+  <li style="position: fixed; bottom: 0; width: 14.4%; background-color: #f8f9fa; padding: 13px; border-top: 1px solid #ddd;">
+    <span id="currentDateTime" style="font-size: 13px; font-weight: bold; font-family: Georgia, serif;"></span>
   </li>
 </ul>
+
+<script>
+  function updateDateTime() {
+    const now = new Date();
+    const options = { 
+      weekday: 'long', 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric', 
+      hour: '2-digit', 
+      minute: '2-digit',  
+      hour12: true 
+    };
+    document.getElementById("currentDateTime").innerHTML = now.toLocaleString('en-US', options);
+  }
+
+  setInterval(updateDateTime, 1000); // Update every second
+  updateDateTime(); // Run once immediately
+</script>
