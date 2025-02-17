d8652b6572d702cd0c2391b8dca6d06781550e1d
soobink1 <peronamarie9@gmail.com>
2025-02-17 10:03:00 +0800

    update logo text in header from "Inventory System" to "Dairy Daily"

--- layouts/header.php
+++ layouts/header.php
@@ -16,7 +16,7 @@
   <body>
   <?php  if ($session->isUserLoggedIn(true)): ?>
     <header id="header">
-      <div class="logo pull-left"> Inventory System</div>
+      <div class="logo pull-left"> Dairy Daily</div>
       <div class="header-content">
       
       <div class="pull-right clearfix">
